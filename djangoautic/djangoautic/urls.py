c9a6b77c8703072600dbcbefa3a41232624c1222
jakehdchoi <jakehdchoi@gmail.com>
2018-07-10 00:27:58 +0900

    articles app created and added to setteings

--- djangoautic/djangoautic/urls.py
+++ djangoautic/djangoautic/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('articles/', include('articles.urls')), # when this url is requested, look up articles/urls.py
     path('about/', views.about), # '$' means this is the end, nothing after (find expression)
     path('', views.homepage), # homepage
 ]
