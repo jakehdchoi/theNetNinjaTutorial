a1648d5177ecaf2f1c700445285810116995b7d3
jakehdchoi <jakehdchoi@gmail.com>
2018-07-10 00:55:59 +0900

    models created (basic)

--- djangoautic/articles/models.py
+++ djangoautic/articles/models.py
@@ -1,3 +1,11 @@
+# https://docs.djangoproject.com/en/2.0/ref/models/fields/
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    # add in thumbnail later
+    # add in author later
