d43ad846e298448264dcba7314b88350c721d940
jakehdchoi <jakehdchoi@gmail.com>
2018-07-10 00:05:33 +0900

    project rearrange

--- djangoautic/djangoautic/wsgi.py
+++ djangoautic/djangoautic/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for theNetNinjaTutorial project.
+WSGI config for djangoautic project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "theNetNinjaTutorial.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoautic.settings")
 
 application = get_wsgi_application()
