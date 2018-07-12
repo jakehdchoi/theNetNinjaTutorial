5a2106615c77fd644df5096029d7823527bb2fb7
jakehdchoi <jakehdchoi@gmail.com>
2018-07-13 04:03:59 +0900

    static files + image

--- djangoautic/djangoautic/urls.py
+++ djangoautic/djangoautic/urls.py
@@ -1,8 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +10,5 @@ urlpatterns = [
     path('about/', views.about), # '$' means this is the end, nothing after (find expression)
     path('', views.homepage), # homepage
 ]
+
+urlpatterns += staticfiles_urlpatterns()
