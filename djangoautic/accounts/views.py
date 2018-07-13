a7c7e79d64c7cb041d01a27a0d65aa0831bfe65a
jakehdchoi <jakehdchoi@gmail.com>
2018-07-13 18:53:05 +0900

    login form

--- djangoautic/accounts/views.py
+++ djangoautic/accounts/views.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def signup_view(request):
@@ -14,3 +14,15 @@ def signup_view(request):
         form = UserCreationForm()
 
     return render(request, 'accounts/signup.html', {'formName':form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log in the user
+            return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'accounts/login.html', {'formName':form})
