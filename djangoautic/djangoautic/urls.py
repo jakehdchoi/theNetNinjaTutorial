83ecde5e367b101742992945b8ab3d895450fd05
jakehdchoi <jakehdchoi@gmail.com>
2018-07-13 22:48:44 +0900

    redirecting the homepage, but need little more refines

--- djangoautic/djangoautic/urls.py
+++ djangoautic/djangoautic/urls.py
@@ -5,13 +5,15 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings # importing from settings.py
+from articles import views as article_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about), # '$' means this is the end, nothing after (find expression)
     path('accounts/', include('accounts.urls')),
     path('articles/', include('articles.urls')), # when this url is requested, look up articles/urls.py
-    path('', views.homepage), # homepage
+    path('', article_views.article_list, name='home'), # homepage
 ]
 
 urlpatterns += staticfiles_urlpatterns()
