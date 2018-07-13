76aa71e5de96462afea6449e658128dd220535e3
jakehdchoi <jakehdchoi@gmail.com>
2018-07-13 20:58:29 +0900

    requiring login: added a decorator

--- djangoautic/articles/views.py
+++ djangoautic/articles/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 
 def article_list(request):
     articles = Article.objects.all().order_by('date')
@@ -11,3 +13,7 @@ def article_detail(request, slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', {'article':article})
+
+@login_required(login_url='/accounts/login/')
+def article_create(request):
+    return render(request, 'articles/article_create.html')
