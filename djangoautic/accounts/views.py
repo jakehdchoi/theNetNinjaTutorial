980c0a3cb5a391fbb53be1f067faf5b964682140
jakehdchoi <jakehdchoi@gmail.com>
2018-07-13 18:59:29 +0900

    logging users in

--- djangoautic/accounts/views.py
+++ djangoautic/accounts/views.py
@@ -1,14 +1,16 @@
 
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             # log the user in
+            login(request, user)
             return redirect('articles:list') # in articles/urls.py: app_name and name in urlpatterns
     else:
         form = UserCreationForm()
@@ -20,7 +22,9 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            # log in the user
+            # log the user in
+            user = form.get_user()
+            login(request, user)
             return redirect('articles:list')
     else:
         form = AuthenticationForm()
